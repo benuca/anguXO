4780fe6896cb5efd7e05c52d0fcb52ed40a95ae3
Jordan Szymandera <jordanszymandera@gmail.com>
2021-03-18 18:12:04 -0400

    ui changes

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
   <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
   <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+styles: ['button { width: 100%; height: 100%; font-size: 4em !important; }']
 })
 export class SquareComponent  {
 
