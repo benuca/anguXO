6615623b67facee652b3a321f7f02353a32b4a1d
Jordan Szymandera <jordanszymandera@gmail.com>
2021-03-18 02:55:34 -0400

    built logic of app

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+
+  squares: any[] = [];  
+  isXNext: boolean = false;
+  winner: string | undefined;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.squares = Array(9).fill(null);
+    this.winner = "";
+    this.isXNext = true;
+  }
+
+  get player(){
+    return this.isXNext ? 'X' : 'O';
+  }
+
+  makeMove(squareID: number){
+    if (this.squares[squareID] = null) 
+    {
+      this.squares.splice(squareID, 1, this.player);
+      this.isXNext = !this.isXNext;
+    }
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+
+
+
+}
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+  <button nbButton *ngIf="!value">{{ value }}</button>
+  <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+  <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: [
-  ]
+styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent implements OnInit {
 
-  constructor() { }
+  @Input() value: 'X' | 'O' | undefined;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
 
 }
